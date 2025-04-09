f422eff80f50e9b5e407776598c8c748851fc5ce
Bojan Coric <bojancoric996@gmail.com>
2025-04-09 14:46:03 +0200

    Simplify controller

--- beer-catalog/src/main/java/com/productdock/beercatalog/controller/BeerController.java
+++ beer-catalog/src/main/java/com/productdock/beercatalog/controller/BeerController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,26 +15,14 @@ public class BeerController {
 
     private final BeerService beerService;
 
-    public record BeerResponse(long id, String name, String description, String origin, double abv, String imageUrl) {
-    }
-
-    public record BeerValidationResponse(long id, String name) {
-    }
-
     @GetMapping("beers")
-    public List<BeerResponse> getBeers() {
-        return beerService.getAll().stream()
-                .map(BeerController::toBeerResponse)
-                .collect(Collectors.toList());
+    public List<Beer> getBeers() {
+        return beerService.getAll();
     }
 
-    @GetMapping("beers/{beerId}/validate")
-    public BeerValidationResponse getBeer(@PathVariable long beerId) {
-        Beer beer = beerService.getById(beerId);
-        return new BeerValidationResponse(beer.getId(), beer.getName());
+    @GetMapping("beers/{beerId}")
+    public Beer getBeer(@PathVariable long beerId) {
+        return beerService.getById(beerId);
     }
 
-    private static BeerResponse toBeerResponse(Beer beer) {
-        return new BeerResponse(beer.getId(), beer.getName(), beer.getDescription(), beer.getOrigin(), beer.getAbv(), beer.getImageUrl());
-    }
 }
